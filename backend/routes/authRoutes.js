92947af86372e94267880b0da392a049e48f0e54
BastardAcademic <32774879+bastardacademic@users.noreply.github.com>
2025-01-21 17:28:34 +0000

    Add missing files and improve project structure

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,32 @@
+﻿const express = require('express');
+const { loginUser, registerUser } = require('../controllers/authController');
+const { body, validationResult } = require('express-validator');
+const router = express.Router();
+
+router.post(
+    '/login',
+    [body('email').isEmail(), body('password').isLength({ min: 6 })],
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    },
+    loginUser
+);
+
+router.post(
+    '/register',
+    [body('email').isEmail(), body('password').isLength({ min: 6 })],
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    },
+    registerUser
+);
+
+module.exports = router;
